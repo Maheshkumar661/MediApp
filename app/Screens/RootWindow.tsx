2b6be6cbe0c1e977f2633a17d5a56d1548ca77e5
Mahesh kumar v r <86484493+Maheshkumar661@users.noreply.github.com>
2025-07-20 03:34:22 +0530

    Update RootWindow.tsx

--- app/Screens/RootWindow.tsx
+++ app/Screens/RootWindow.tsx
@@ -1,5 +1,3 @@
-// app/Screens/RootWindow.tsx
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
